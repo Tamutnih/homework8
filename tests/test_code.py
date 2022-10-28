3d6a2affd45d53c6ca05c67d0b8742f8445cd71a
Tatiana <109300178+Tamutnih@users.noreply.github.com>
2022-10-28 14:29:05 +0500

    Add files via upload

---
+++ tests/test_code.py
@@ -0,0 +1,33 @@
+# 1. Напишите программу, удаляющую из
+# текста все слова содержащие "абв".
+
+import Unittest
+from file
+txt = input(f"Enter text:\n")
+
+
+def del_some_words(some_txt):
+    some_txt = list(filter(lambda x: 'абв' not in x, txt.split()))
+    return " ".join(my_text)
+
+
+my_text = del_some_words(txt)
+print(txt)
+
+
+def get_words(param):
+    pass
+
+
+def test_get_words():
+    """тест 1"""
+    assert get_words('Entered text has no words') ==\
+        12345, 'Wrong data in text'
+
+
+def test_get_wrong_data():
+
+
+if __name__ == "__main__":
+test_get_wrong_data():
+test_get_words_empty():
